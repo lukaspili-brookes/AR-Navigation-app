f7f74ee4210b647b814127ac979922f08bb085b2
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2013-07-05 23:27:59 +0200

    Loginfoview.

--- app/src/main/java/com/siu/android/arapp/view/LogInfoView.java
+++ app/src/main/java/com/siu/android/arapp/view/LogInfoView.java
@@ -1,7 +1,33 @@
 package com.siu.android.arapp.view;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.siu.android.arapp.R;
+
 /**
  * Created by lukas on 7/5/13.
  */
-public class LogInfoView {
+public class LogInfoView extends RelativeLayout {
+
+    public LogInfoView(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public LogInfoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
+
+    public LogInfoView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initView(context);
+    }
+
+    private void initView(Context context) {
+        View.inflate(context, R.layout.log_info_view, this);
+    }
 }
